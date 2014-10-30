2b17b1c2e24501a35a66cc7353878d1c31527e66
catscs <catscs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 20:57:11 +0000

    actulizar
    git-svn-id: https://plugins.svn.wordpress.org/suscriptflech/trunk@1017003 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/temawidget.php
+++ templates/temawidget.php
@@ -1,5 +1,4 @@
 <form name="wp_id_suscriptflech">
-		<img id="carga" style="display:none" src="<?php echo plugins_url('suscriptflech/imagenes/cargando.gif'); ?>" widht="16" height="16">
 				<div>
 				<p><input name="wp_email_sucriptflech" id="wp_email_sucriptflech" type="email"  class="regular-text form-control" pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" placeholder="E-mail" required> 
 
@@ -10,5 +9,6 @@
 			<div class="aviso1" id="aviso1">El email ya esta en uso</div>
 			<div class="aviso3" id="aviso3"></div>
 			<div class="aviso2" id="aviso2">Tienes que escribir un email</div>
+			<img id="carga" style="display:none" src="<?php echo plugins_url('suscriptflech/imagenes/cargando.gif'); ?>">
 	
 </form>
